a158af228937da40c08fbe2b1bcceceb5aa7cf9c
Vados test <vadimyarda27@gmail.com>
2024-08-15 12:08:23 +0300

    HW

---
+++ src/com/company/Netology/Ints.java
@@ -0,0 +1,8 @@
+package com.company.Netology;
+
+public interface Ints {
+    int sum(int arg0, int arg1);
+    int mult(int arg0, int arg1);
+    int pow(int a, int b);
+}
+
---
+++ src/com/company/Netology/Main.java
@@ -0,0 +1,11 @@
+package com.company.Netology;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.mult(3,4));
+        System.out.println(calc.pow(8,4));
+        System.out.println(calc.sum(6,8));
+    }
+}
